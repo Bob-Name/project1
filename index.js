362a6641a4b081a7fed4089a89790c4e5c1c6b20
Hopny <1078054714@qq.com>
2018-06-14 10:24:05 +0800

    hopny 修改了js文件

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var oLi=document.getElementsByTagName("li");
 for(var i=0;i<oLi.length;i++){
     oLi[i].index=i
     oLi[i].onclick=function(){
-        
-    }
+        console.log("腾腾是个二傻子");
+}
 }
