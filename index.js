aa09a84be8863c0722db008585fa3da72d2a9b29
teng <1474218225@qq.com>
2018-06-15 08:40:30 +0800

    在分支2上修改了index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var oLi=document.getElementsByTagName("li");
+var oBox=document.getElementsByClassName("box");
+var a=0;
 for(var i=0;i<oLi.length;i++){
     oLi[i].index=i
     oLi[i].onclick=function(){
-        console.log("腾腾是个二傻子");
-}
+       for(var j=0;j<oLi.length;j++){
+       	oLi[j].className="";
+       	oBox[j].style.display="none";
+       }
+       oLi[this.index].className="active"
+       oBox[this.index].style.display="block"
+  }
 }
